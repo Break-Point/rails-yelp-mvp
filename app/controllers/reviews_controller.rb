fde66ca2f5695fa20a61fdfafd0d081d193f8c57
Thomas Loubser <taloubser@gmail.com>
2021-04-25 10:09:27 +0100

    trying to update reveiw render

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,8 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      # raise
+      render 'restaurants/show'
     end
   end
 
